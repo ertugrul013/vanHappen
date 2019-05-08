45b65420ee3fc2f73ac26715588fac752f547395
ertugrul <ertugrul013@hotmail.com>
2019-05-08 14:30:45 +0200

    finished the trash class

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Trash
+{
+    public enum Type
+    {
+        green,
+        plastic,
+        chemicals,
+        paper
+    }
+
+    public Type mytype;
+    public float spawnTime, pickupTime;
+    
+    /// <summary>
+    /// used for determing when the object has spawned and pickedup
+    /// </summary>
+    /// <returns>current time</returns>
+    public float SetTime()
+    {
+        return (Time.time);
+    }
+}
