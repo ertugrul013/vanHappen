c8d2f8819862ca634d07934d8c7e287417800d91
ertugrul <ertugrul013@hotmail.com>
2019-05-11 19:33:44 +0200

    setup the function for getting touch input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,38 @@
+using System;
+using UnityEditor.Animations;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class PlayerController : MonoBehaviour
 {
-    public class PlayerController : MonoBehaviour
+    private AnimatorController _animatorController;
+    [SerializeField] private float swipeThreshold;
+    
+    private void GetSwipe()
     {
-        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 deltaPosition = Input.GetTouch(0).deltaPosition;
+
+            if (deltaPosition.x > swipeThreshold)
+            {
+                //move to the right
+            }
+            else if (deltaPosition.x < swipeThreshold)
+            {
+                
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("obstacle"))
+        {
+            
+        }
+        else if (col.gameObject.CompareTag("pickup"))
+        {
+            Gamemanager.instance.TrashOrderTracking(col.gameObject);
+        }
     }
 }
