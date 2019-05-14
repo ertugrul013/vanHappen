982a425e15c3886294a9deedd6a45897d9d98049
ertugrul <ertugrul013@hotmail.com>
2019-05-14 22:50:56 +0200

    cleanup the class

--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -4,31 +4,29 @@ using UnityEngine;
 [Serializable]
 public class Trash
 {
-    public enum Type
-    {
-        green,
-        plastic,
-        chemicals,
-        paper
-    }
+	public enum Type
+	{
+		green,
+		plastic,
+		chemicals,
+		paper
+	}
 
-    public Type mytype;
-    public float spawnTime, pickupTime;
-    private bool isSpawned = false;
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    public void SetTime()
-    {
-        if (isSpawned)
-        {
-            pickupTime = Time.time - spawnTime;
-        }
-        else if(!isSpawned)
-        {
-            spawnTime = Time.time;
-            isSpawned = true;
-        }
-    }
+	private bool isSpawned;
+
+	public Type mytype;
+	public float spawnTime, pickupTime;
+
+	public void SetTime()
+	{
+		if (isSpawned)
+		{
+			pickupTime = Time.time - spawnTime;
+		}
+		else if (!isSpawned)
+		{
+			spawnTime = Time.time;
+			isSpawned = true;
+		}
+	}
 }
