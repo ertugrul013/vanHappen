01bd9da7c6a227292544c592efcc90071810fc60
ertugrul <ertugrul013@hotmail.com>
2019-05-15 09:41:44 +0200

    debug prefabs

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -1,59 +1,71 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
 {
-    public static Gamemanager instance;
-    
-    [SerializeField] private GameObject world;
-    [SerializeField][Range(40,70)] private float worldSpeed;
-  
-    private int currentLifes = 3;
-    
-    private List<Trash> trash = new List<Trash>();
-    [SerializeField] private GameObject _trashObject;
+	public static Gamemanager instance;
 
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this);
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }
+	private readonly List<Trash> trash = new List<Trash>();
 
-    private void FixedUpdate()
-    {
-        WorldSpinning();
-    }
+	private int _currentLifes = 3;
+	[SerializeField] private Vector2[] spawmLocations = new Vector2[3];
+	public float spawnRate, basetime;
+	[SerializeField] private GameObject[] trashObject;
 
-    public void LifeTracking()
-    {
-        
-    }
-    
-    public void TrashOrderTracking(GameObject t)
-    {
-        var ct = t.GetComponent<Trash>();
-        ct.SetTime();
-        trash.Add(ct);
-        Destroy(t);
-    }
+	[SerializeField] private GameObject world;
+	[SerializeField] [Range(40, 70)] private float worldSpeed;
 
-    void WorldSpinning()
-    {
-        world.transform.Rotate(worldSpeed * Time.deltaTime,0,0,Space.Self);
-    }
-    
-   
-    void SpawnObstacles()
-    {
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(this);
+			basetime = spawnRate;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
 
-    }
-}
-  
+	private void FixedUpdate()
+	{
+		WorldSpinning();
+		SpawnObstacles();
+	}
+
+	public void LifeTracking()
+	{
+		_currentLifes--;
+		if (_currentLifes == 0) SceneManager.LoadScene(4);
+	}
+
+	public void TrashOrderTracking(GameObject t)
+	{
+		var ct = t.GetComponent<Trash>();
+		ct.SetTime();
+		trash.Add(ct);
+	}
+
+	private void WorldSpinning()
+	{
+		world.transform.Rotate(worldSpeed * Time.deltaTime, 0, 0, Space.Self);
+	}
+
+
+	private void SpawnObstacles()
+	{
+		spawnRate -= 1 * Time.deltaTime;
+
+		if (spawnRate <= 0)
+		{
+			var i = Random.Range(0, 3);
+			var j = Random.Range(0, 3);
+			var obs = Instantiate(trashObject[j], spawmLocations[i], Quaternion.identity);
+			obs.transform.SetParent(world.transform, true);
+			TrashOrderTracking(obs);
+			spawnRate = basetime;
+		}
+	}
