20cb7886de7026abe6bba961017faafdc7e07435
ertugrul <ertugrul013@hotmail.com>
2019-06-06 13:58:37 +0200

    added play and pause funcinality

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,6 +5,20 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
 	private Image[] LifeUiElements;
+
+	[Header("pause control")]
+	[SerializeField] private Image pausePlayButt;
+	[SerializeField] private Sprite[] StateSprite = new Sprite[2];
+	private bool isPaused;
+	
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		isPaused = true;
+	}
+	
 	public void LoadScene(int index)
 	{
 		SceneManager.LoadScene(index);
@@ -13,5 +27,20 @@ public class UIController : MonoBehaviour
 	public void SetLifeUI(int amountOfLifes)
 	{
 		
+	}
+
+	public void Pause()
+	{
+		if (isPaused)
+		{
+			pausePlayButt.sprite = StateSprite[0];
+			Time.timeScale = 0;
+		}
+		else if (!isPaused)
+		{
+			pausePlayButt.sprite = StateSprite[1];
+			Time.timeScale = 1;
+		}
+		isPaused = !isPaused;
 	}
 }
